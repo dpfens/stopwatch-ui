52da960582d374a1b55ebff36665cc94828369ef
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2025-08-15 02:21:20 -0400

    moves header into it's own component

---
+++ src/app/components/shared/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject} from '@angular/core';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    RouterLink, RouterLinkActive,
+    MatToolbarModule, MatButtonModule, MatIconModule,
+    MatFormFieldModule, MatInputModule, MatAutocompleteModule,
+    FormsModule, ReactiveFormsModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  private formBuilder = inject(FormBuilder);
+  searchForm = this.formBuilder.group({
+    query: '',
+  })
+}
