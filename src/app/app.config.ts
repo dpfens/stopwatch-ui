28b50169ff74f529b49d3bd0bd3a277b089a8109
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2025-09-30 00:25:00 -0400

    adds Google Analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { GoogleAnalyticsService } from './services/analytics/google-analytics.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideAppInitializer(() => {
+      const gaService = inject(GoogleAnalyticsService);
+      gaService.initialize('G-XXXXXXXXXX'); // Your GA4 measurement ID
+    })
   ]
 };
